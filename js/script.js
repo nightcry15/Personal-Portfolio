2da19eb56890a38e2fa1602db55ec0d350ec16ea
Alexander Searle <41946602+nightcry15@users.noreply.github.com>
2023-04-03 16:42:14 +0200

    Final-ish

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx.-x');
+    menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 }
 
@@ -32,10 +32,28 @@ window.onscroll = () => {
     header.classList.toggle('sticky', window.scrollY > 100);
 
     /*Remove Toggle Icon and Navbar when click  on Navbar Link (scroll)*/
-    menuIcon.classList.remove('bx.-x');
+    menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 };
 
+/*Scroll reveal*/
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+ });
 
+ ScrollReveal().reveal('.home-content, .heading', { origin: 'top'});
+ ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom'});
+ ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left'});
+ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right'});
 
-
+/*Typed js*/
+const typed = new Typed('.multiple-text', {
+    strings: ['Photographer', 'Gamer', '....Help'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
